63ceab15a915f5cfcf58ec0f032759267cdfa210
“[Etienne <[etienne.pautet@gmail.com]>
2024-03-06 15:19:41 -0300

    Second commit

---
+++ EX-2.py
@@ -0,0 +1,9 @@
+#Exercício 2
+
+nota1 = float(input("Informe a primeira nota:\n"))
+nota2 = float(input("Informe a segunda nota:\n"))
+nota3 = float(input("Informe a terceira nota:\n"))
+nota4 = float(input("Informe a quarta nota:\n"))
+nota5 = float(input("Informe a quinta nota:\n"))
+
+print("A média é igual a:", (nota1 + nota2 + nota3+ nota4 + nota5) / 5)
